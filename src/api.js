a41705023c7c13fd29da64f39a78908cfd8e0aa2
Ömer Ali Uygun <112541841+omerali1996@users.noreply.github.com>
2025-10-08 12:55:34 +0300

    Delete src directory

--- src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { BACKEND_URL } from "./config";
-
-const api = axios.create({
-  baseURL: BACKEND_URL,
-  timeout: 10000,
-});
-
-export default api;
--- src/config.js
+++
@@ -1,4 +0,0 @@
-// Eğer create-react-app kullanıyorsan .env içine REACT_APP_BACKEND_URL yazabilirsin.
-// process.env.REACT_APP_BACKEND_URL öncelikli olacak, yoksa default kullanılacak.
-export const BACKEND_URL =
-  process.env.REACT_APP_BACKEND_URL || "https://acil-servis-quiz-version2.onrender.com";
